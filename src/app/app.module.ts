6d8c0b166a5bb9c5ca299135fbc1560a460cf72f
Mykolas <mykolas.kucinskas2@gmail.com>
2022-12-17 23:46:50 +0200

    added main components and front page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { EmployeeslistComponent } from './employeeslist/employeeslist.component';
+import { CategorylistComponent } from './categorylist/categorylist.component';
+import { NewemployeeComponent } from './newemployee/newemployee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainpageComponent,
+    EmployeeslistComponent,
+    CategorylistComponent,
+    NewemployeeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/categorylist/categorylist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorylist',
+  templateUrl: './categorylist.component.html',
+  styleUrls: ['./categorylist.component.css']
+})
+export class CategorylistComponent {
+
+}
